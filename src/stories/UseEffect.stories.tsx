db4a7de0701865bc7188649cdc7c732fb2c9b34f
Ilya030290 <anoshko90@mail.ru>
2022-04-11 14:45:54 +0300

    Add useEffect stories

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react";
+
+
+export default {
+    title: 'useEffect demo'
+}
+
+export const SimpleExample = () => {
+
+    const [counter, setCounter] = useState(1);
+    const [value, setValue] = useState(1);
+
+    console.log("Simple Example");
+    // Примеры срабатывания useEffect
+    useEffect(() => {
+        console.log("useEffect render every time");
+    })
+
+    useEffect(() => {
+        console.log("useEffect render only first Time");
+    }, [])
+
+    useEffect(() => {
+        console.log("useEffect first render and every time when was changed counter");
+    }, [counter])
+
+    useEffect(() => {
+        console.log("useEffect first render and every time when was changed value");
+    }, [value])
+
+    return (
+        <>
+            <div>
+                Hello Value: {value}
+                <button onClick={() => setValue(value * 2)}>Value x2</button>
+            </div>
+            <div>
+                Hello Counter: {counter}
+                <button onClick={() => setCounter(counter + 1)}>Counter +</button>
+            </div>
+        </>
+    )
+
+}
