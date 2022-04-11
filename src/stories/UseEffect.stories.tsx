cce6718ef35c9c51bd18b3c140209251df44f3ec
Ilya030290 <anoshko90@mail.ru>
2022-04-11 16:00:50 +0300

    UseEffect stories with setTimeout and setInterval Example

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -42,3 +42,38 @@ export const SimpleExample = () => {
     )
 
 }
+
+export const SetTimeoutExample = () => {
+
+    const [counter, setCounter] = useState(1);
+    const [value, setValue] = useState(1);
+
+    console.log("SetTimeout Example");
+
+   useEffect(() => {
+       setTimeout(() => {
+           console.log("SetTimeout");
+       }, 1500);
+    }, [value])
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log("SetInterval");
+            setCounter((state) => state + 1);
+        }, 2000);
+    }, [])
+
+
+    return (
+        <>
+            <div>
+                Hello Value: {value}
+                <button onClick={() => setValue(value * 2)}>Value x2</button>
+            </div>
+            <div>
+                Hello Counter: {counter}
+            </div>
+        </>
+    )
+
+}
