36f5bce019b635bdf04150d5b4c4b184bab02751
Ilya030290 <anoshko90@mail.ru>
2022-04-24 18:42:16 +0300

    Add ResetEffectExample and KeysTrackerExample for UseEffect.stories

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -51,16 +51,24 @@ export const SetTimeoutExample = () => {
     console.log("SetTimeout Example");
 
    useEffect(() => {
-       setTimeout(() => {
+
+       const timeoutId = setTimeout(() => {
            console.log("SetTimeout");
        }, 1500);
+
+       return clearTimeout(timeoutId);
     }, [value])
 
     useEffect(() => {
-        setInterval(() => {
+        const intervalId = setInterval(() => {
+
             console.log("SetInterval");
             setCounter((state) => state + 1);
         }, 2000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
     }, [])
 
 
@@ -76,4 +84,52 @@ export const SetTimeoutExample = () => {
         </>
     )
 
+}
+
+export const ResetEffectExample = () => {
+
+    const [counter, setCounter] = useState<number>(1);
+
+    console.log("Component rendered");
+
+    useEffect(() => {
+        console.log("Effect occurred:" + counter);
+        return () => {
+            console.log("RESET Effect")
+        }
+    }, [counter]);
+
+    const increase =() => {
+        setCounter(counter + 1);
+    }
+
+    return (
+      <>
+          Hello, counter: {counter} <button onClick={increase}>+</button>
+      </>
+    );
+}
+
+export const KeysTrackerExample = () => {
+
+    const [text, setText] = useState<string>('');
+
+    console.log("Component rendered with" + text);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key);
+            setText(text + e.key);
+        }
+        window.addEventListener('keypress', handler)
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, [text]);
+
+    return (
+        <>
+            Typed text: {text}
+        </>
+    );
 }
